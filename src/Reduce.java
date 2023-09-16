39674f3ad5720a4f9a5262a09124291841da6c60
irtiza-ahmed <irtiza.ahmed@mail.utoronto.ca>
2023-09-16 14:12:22 -0400

    refactored to extract helper method

--- src/Reduce.java
+++ src/Reduce.java
@@ -2,11 +2,10 @@ public class Reduce {
     public static void main(String[] args) {
         int i = 100;
         int count = 0;
-        while (i > 0){
+        while (i > 0) {
             if (i % 2 == 0) {
                 i = i / 2;
-            }
-            else {
+            } else {
                 i = i - 1;
             }
             count++;
